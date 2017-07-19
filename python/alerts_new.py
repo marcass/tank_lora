55852a16fb4ce13c6d0579051d8d8d1e0dd7f332
Marcass <best.nose@gmail.com>
2017-07-19 21:26:15 +1200

    fixed many bugs. many more to go. rrd plotting now working. replying to status requests in the most verbose way possible

--- python/alerts_new.py
+++ python/alerts_new.py
@@ -12,6 +12,7 @@ import creds
 from pprint import pprint
 import json, ast
 import os.path
+import rrdtool
 
 class Tanks:
     def __init__(self, name, min_vol, url):
@@ -21,7 +22,7 @@ class Tanks:
         self.statusFlag = 'OK'
         self.url = url
         self.rrdpath = '/home/pi/git/tank_lora/python/mqtt2rrd/rrd/'
-        self.rrd_file = rrdpath +name +'.rrd'
+        self.rrd_file = self.rrdpath +name +'.rrd'
     
 #t = Tanks("top",   200.0, 'https://thingspeak.com/channels/300940/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Top+tank&type=line&xaxis=Time&yaxis=Tank+volume+%28l%29')
 #n = Tanks("noels", 150.0, 'https://thingspeak.com/channels/300940/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Noel%27s+break+tank&type=line&xaxis=Time&yaxis=Volume+%28l%29')
@@ -33,6 +34,10 @@ n = Tanks("noels", 150.0, 'https://thingspeak.com/channels/300940')
 s = Tanks("sals",  150.0, 'https://thingspeak.com/channels/300940')
 x = Tanks("test",  150.0, 'oops')
 
+tanks_by_topic = {tank.waterTop : tank for tank in [t,n,s,x]}
+tanks_by_name = {tank.name : tank for tank in [t,n,s,x]}
+tank_list = [t,n,s,x]
+
 tsL = 'https://thingspeak.com/channels/300940'
 
 class Keyboard:
@@ -40,17 +45,17 @@ class Keyboard:
         #disp = single alert, multi alert, graph request, help etc
         self.version = version
         
-    def format_keys(self, tankID=0):
+    def format_keys(self, tank_instance=0):
         if self.version == 'status':
-            if tankID.statusFlag == 'OK':
+            if tank_instance.statusFlag == 'OK':
                 keyboard = InlineKeyboardMarkup(inline_keyboard=[[
-                        InlineKeyboardButton(text='Get ' +tankID +' graph', callback_data='fetch graph'),
+                        InlineKeyboardButton(text='Get ' +tank_instance.name +' graph', callback_data='fetch graph'),
                         ]])
                 
             else:
                 keyboard = InlineKeyboardMarkup(inline_keyboard=[[
-                        InlineKeyboardButton(text='Reset ' +tankID, callback_data='reset_alert'),
-                        InlineKeyboardButton(text='Get ' +tankID +' graph', callback_data='fetch graph'),
+                        InlineKeyboardButton(text='Reset ' +tank_instance.name, callback_data='reset_alert'),
+                        InlineKeyboardButton(text='Get ' +tank_instance.name +' graph', callback_data='fetch graph'),
                         ]])
         elif self.version == 'helpMe':
             keyboard = InlineKeyboardMarkup(inline_keyboard=[[
@@ -59,7 +64,7 @@ class Keyboard:
                         ]])
         elif self.version == 'alert':
             keyboard = InlineKeyboardMarkup(inline_keyboard=[[
-                        InlineKeyboardButton(text='Get link to ' +tankID +' graph', callback_data='graph'),
+                        InlineKeyboardButton(text='Get link to ' +tank_instance.name +' graph', callback_data='graph'),
                         ]])
         else:
             keyboard = InlineKeyboardMarkup(inline_keyboard=[[
@@ -71,61 +76,50 @@ h = Keyboard('helpMe')
 st = Keyboard('status')
 a = Keyboard('alert')
     
-#find the goddamn tank instance that is incoming    
-def sort(val):
-    for g in [t,n,s,x]:
-        d = g.__dict__
-        for k, v in d.items():
-            if v == val:
-                #print g.name +' tank identified'
-                return g
-            #else:
-                #print 'fail'
-#may need to handle fails here somehow
-    
-def status_mess():
-    for y in [t,n,s,x]:
-        print y.name +' is ' +y.statusFlag
+def status_mess(tank_instance):
+    ret = tank_instance.name+' is '+tank_instance.statusFlag +'\n'
+    return ret
         
 
 def on_chat_message(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     text = msg['text']
-    if '/help' or '/Help' in text:
-        message = bot.sendMessage(creds.groupID, 'Send /Status to see all tanks status', reply_markup=h.format_keys())
-    elif '/status' or '/Status' in text:
-        for i in [t,n,s,x]:
-            message = bot.sendMessage(creds.groupID, i.name +' status as requsted '
-                                   +status_mess() , reply_markup=st.format_keys())
+    if ('/help' in text) or ('/Help' in text):
+        message = bot.sendMessage(creds.group_ID, 'Send /Status to see all tanks status', reply_markup=h.format_keys())
+    elif ('/status' in text) or ('/Status' in text):
+        print 'IM AM HERE'
+        for tank in tank_list:
+            message = bot.sendMessage(creds.group_ID, 
+                'Status as requested \n'\
+                + status_mess(tank) ,
+            reply_markup=st.format_keys(tank))
     else:
-        message = bot.sendMessage(creds.groupID, 'Bugger off, that does nothing. Send /help instead', reply_markup=h.format_keys())
+        message = bot.sendMessage(creds.group_ID, 'Bugger off, that does nothing. Send /help instead', reply_markup=h.format_keys())
 
 def on_callback_query(msg):
     query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
     print('Callback Query:', query_id, from_id, query_data)
     print 'printing message now'
-    print msg
     mess = msg['message']['text']     #pull text from message
     print mess
-    tank = mess.split(' ')[0]         #split message on spaces and get first member (string of tank's name)
-    print 'tank is ' +tank
-    tank = sort(tank)
-    print tank.name +' is about to get actioned'
-    #callbacks for 'reset_alert' 'thingspeak link' 'fetch graph'
-    if query_data == 'reset_alert':
-        print tank.name +' ' +tank.statusFlag
-        tank.statusFlag = 'OK'
-        print tank.statusFlag
-        #timer.cancel()
-        bot.answerCallbackQuery(query_id, text='Alert now reset')
-    elif query_data == 'fetch graph':
-        graph = bot.sendMessage(creds.groupID, tank.url, reply_markup=a.format_keys(tank.name))
-        bot.answerCallbackQuery(query_id, text='Here you go (so demanding)') 
-    elif query_data == 'thingspeak link':
-        graph = bot.sendMessage(creds.groupID, tsL, reply_markup=h.format_keys(tank.name))
-        bot.answerCallbackQuery(query_id, text='Here you go (so demanding)')
-    elif query_data == 'help':
-        bot.sendMessage(creds.groupID, 'Send "/help" for more info', reply_markup=h.format_keys(tank.name))
+    tank_name = mess.split(' ')[0]         #split message on spaces and get first member   
+    if tanks_by_name.has_key(tank_name):
+        tank = tanks_by_name[tank_name]
+        #callbacks for 'reset_alert' 'thingspeak link' 'fetch graph'
+        if query_data == 'reset_alert':
+            print tank.name +' ' +tank.statusFlag
+            tank.statusFlag = 'OK'
+            print tank.statusFlag
+            #timer.cancel()
+            bot.answerCallbackQuery(query_id, text='Alert now reset')
+        elif query_data == 'fetch graph':
+            graph = bot.sendMessage(creds.group_ID, tank.url, reply_markup=a.format_keys(tank.name))
+            bot.answerCallbackQuery(query_id, text='Here you go (so demanding)') 
+        elif query_data == 'thingspeak link':
+            graph = bot.sendMessage(creds.group_ID, tsL, reply_markup=h.format_keys(tank.name))
+            bot.answerCallbackQuery(query_id, text='Here you go (so demanding)')
+        elif query_data == 'help':
+            bot.sendMessage(creds.group_ID, 'Send "/help" for more info', reply_markup=h.format_keys(tank.name))
 
 TOKEN = creds.botAPIKey
 botID = creds.bot_ID
@@ -145,38 +139,35 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     #print(msg.topic+' '+msg.payload)
     vol = float(msg.payload)
-    in_tank = sort(msg.topic)
+    in_tank = tanks_by_topic[msg.topic]
     #print in_tank.name
     print in_tank.name +' tank message incoming ' + 'minimum vol = ' +str(in_tank.min_vol) +' actual volume = ' +str(vol)
     if vol < in_tank.min_vol:
         print in_tank.name +' under thresh'
         if in_tank.statusFlag == 'OK':
             in_tank.statusFlag = 'bad'
-            #put graph in here as well https://notroot.wordpress.com/2010/03/22/python-rrdtool-tutorial/
-            ret = rrdtool.graph( rrdpath +"net.png", "--start", "-1d", "--vertical-label=Bytes/s",
-                "DEF:inoctets=" +in_tank.rrd_file +":input:AVERAGE",
-                "DEF:outoctets=" +in_tank.rrd_file +":output:AVERAGE",
-                #"AREA:inoctets#00FF00:In traffic",
-                "LINE1:outoctets#0000FF:" +in_tank.name +"\r",
-                "CDEF:inbits=inoctets,8,*",
-                "CDEF:outbits=outoctets,8,*",
-                "COMMENT:\n",
-                "GPRINT:inbits:AVERAGE:Avg water level: %6.2lf %Sbps",
-                "COMMENT:  ",
-                "GPRINT:inbits:MAX:Max water level: %6.2lf %Sbps\r",
-                #"GPRINT:outbits:AVERAGE:Avg Out traffic: %6.2lf %Sbps",
-                #"COMMENT: ",
-                #"GPRINT:outbits:MAX:Max Out traffic: %6.2lf %Sbps\r"
-                )
+            
+            generate_png(in_tank)
             # perform action required to send image with data
-            send_graph = bot.sendPhoto((creds.groupID, open(rrdpath +'net.png'), in_tank.name +' tank')
-            send = bot.sendMessage(creds.groupID, in_tank.name +' tank is low', reply_markup=a.format_keys(in_tank.name))
+            send_graph = bot.sendPhoto(creds.group_ID, open(in_tank.rrdpath +'net.png'), in_tank.name +' tank')
+            send = bot.sendMessage(creds.group_ID, in_tank.name +' tank is low', reply_markup=a.format_keys(in_tank.name))
         elif in_tank.statusFlag == 'bad':
             print 'ignoring low level'
         else:
             print 'status flag error'        
     else:
         print 'level fine, doing nothing'
+
+def generate_png(tank_instance):
+    #put graph in here as well https://notroot.wordpress.com/2010/03/22/python-rrdtool-tutorial/
+    ret = rrdtool.graph(tank_instance.rrdpath +"net.png",\
+                        "--start", "-1d",\
+                        "--vertical-label=Liter",\
+                        "-w 400",\
+                        "-h 200",\
+                    
+        'DEF:f='+tank_instance.rrd_file+':temp:AVERAGE', \
+        'LINE1:f#0000ff:'+tank_instance.name+' Water')
 
 #subscribe to broker and test for messages below alert values
 client = mqtt.Client()
