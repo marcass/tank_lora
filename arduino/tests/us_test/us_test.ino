d7d6e4938e5108a56085d4bb43b01b02e9e84d8d
Marcass <best.nose@gmail.com>
2017-07-26 23:02:52 +1200

    ultrasound test

---
+++ arduino/tests/us_test/us_test.ino
@@ -0,0 +1,52 @@
+
+#include <SPI.h>
+#include <LoRa.h>
+#include <avr/interrupt.h>
+#include <NewPing.h>
+
+#define POWER 3
+#define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 200 
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+int counter = 0;
+int dist;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(POWER, INPUT);
+  digitalWrite(POWER, LOW);
+  Serial.println("LoRa Sender");
+  LoRa.setPins(1, 4, 7); //reset is pd4, adc8, dio0 is P(ort)E 6 or int6
+
+  if (!LoRa.begin(433E6)) { // initialise at 433MHz
+    Serial.println("Starting LoRa failed!");
+    while (1);
+    //record current state of analogue read pins
+    
+    
+  }
+}
+
+void loop() {
+  delay(1000);
+  //READ MESSAGE
+  //digitalWrite(POWER, HIGH);
+  //delay(10);
+  dist = sonar.ping_cm();
+  //digitalWrite(POWER, LOW);
+  // send packet
+  LoRa.beginPacket();
+  LoRa.print("Ping = ");
+  LoRa.print(sonar.ping_cm());
+  LoRa.print("cm");  
+  LoRa.print(": count = ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  counter++;
+}
+
+
+
