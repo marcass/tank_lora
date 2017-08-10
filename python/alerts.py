f6c9a88e6293ece9c88283f0254cc71111840c45
Marcus <best.nose@gmail.com>
2017-08-11 10:54:35 +1200

    title cleanup on plots

--- python/alerts.py
+++ python/alerts.py
@@ -122,34 +122,36 @@ def plot_tank(tank, period, vers, target_id):
     fig, ax = plt.subplots()
     if vers == 'water':
         data = 'water_volume'
-        label = 'volume (l)'
+        label = 'Volume (l)'
     if vers == 'batt':
         data = 'voltage'
-        label = data
+        label = 'Battery Voltage'
     if type(tank) is list:
         title_name = ''
         for i in tank:
             d = query_via_tankid(i.nodeID, period)
             ax.plot_date(d['timestamp'],d[data], i.line_colour, label=i.name, marker='o', markersize='5')
             title_name += ' '+i.name
-            ax.set(xlabel='time', ylabel=label, title='Tanks '+data)
+            ax.set(xlabel='Datetime', ylabel=label, title='Tanks '+label)
+        title_name += ' plot'
     else:
         d = query_via_tankid(tank.nodeID, period)
         if vers == 'bi_plot':
             title_name = 'Water Level and Voltage for '+tank.name+' Tank'
-            ax.plot_date(d['timestamp'],d['water_volume'], 'b', label='Water Volume',  marker='o', markersize='5')
+            ax.plot_date(d['timestamp'],d['water_volume'], 'b', label='Water Volume (l)',  marker='o', markersize='5')
             ax.set_xlabel('Time')
             # Make the y-axis label, ticks and tick labels match the line color.
             ax.set_ylabel('Water Volume', color='b')
             ax.tick_params('y', colors='b')
             ax2 = ax.twinx()
-            ax2.plot_date(d['timestamp'],d['voltage'], 'r', label='Voltage', marker='p', markersize='5')
+            ax2.plot_date(d['timestamp'],d['voltage'], 'r', label='Voltage (V)', marker='p', markersize='5')
             ax2.set_ylabel('Voltage', color='r')
             ax2.tick_params('y', colors='r')
         else:
-            title_name = tank.name
+            title_name = tank.name+' plot'
             ax.plot_date(d['timestamp'],d[data], tank.line_colour, label=tank.name, marker='o', markersize='5')
-            ax.set(xlabel='time', ylabel=label, title=tank.name+' '+data)
+            ax.set(xlabel='Datetime', ylabel=label, title=tank.name+' '+data)
+            plt.axhspan(tank.min_vol, tank.calced_vol, facecolor='#1f77b4', alpha=0.3)
     ax.get_xaxis().set_major_formatter(format_date)
     #times = ax.get_xticklabels()
     #plt.setp(times, rotation=30)       
@@ -158,7 +160,8 @@ def plot_tank(tank, period, vers, target_id):
     plt.tight_layout()
     fig.savefig(tanks.tank_list[0].pngpath+'net.png')
     plt.close()
-    send_graph = bot.sendPhoto(target_id, open(tanks.tank_list[0].pngpath +'net.png'), title_name)
+    #send_graph = bot.sendPhoto(target_id, open(tanks.tank_list[0].pngpath +'net.png'), title_name)
+    send_graph = bot.sendPhoto(target_id, open(tanks.tank_list[0].pngpath +'net.png'))
     
 def status_mess(tag, chat_id):
     if tag == 'all':
