5ea7eb344d948fc64b3d6615c21b988d09caa610
Marcus <best.nose@gmail.com>
2018-05-30 09:41:37 +1200

    updated uwsgi ing

--- tank_monitor/src/main.js
+++ tank_monitor/src/main.js
@@ -5,8 +5,9 @@ import App from './App'
 // import AppNav from './components/AppNav'
 import router from './router'
 import VueAuth from '@websanova/vue-auth'
-
-// const BASE_URL = 'https://skibo.duckdns.org/tanktestapi'
+// import BASE_URL from '../utils/tank-api'
+const BASE_URL = 'https://skibo.duckdns.org/tanktestapi'
+// export { BASE_URL }
 
 Vue.router = router
 
@@ -17,11 +18,12 @@ Vue.use(VueAuth, {
   auth: require('./bearer.js'),
   http: require('@websanova/vue-auth/drivers/http/axios.1.x.js'),
   router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js'),
-  loginData: {url: 'https://skibo.duckdns.org/tanktestapi/auth/login', method: 'POST', redirect: {name: 'TankLord'}},
+  // loginData: {url: 'https://skibo.duckdns.org/tanktestapi/auth/login', method: 'POST', redirect: {name: 'TankLord'}},
+  loginData: {url: BASE_URL + '/auth/login', method: 'POST', redirect: {name: 'TankLord'}},
   // refreshData: {enabled: false},
-  refreshData: {url: 'https://skibo.duckdns.org/tanktestapi/auth/refresh', method: 'POST', enabled: true, interval: 30},
+  refreshData: {url: BASE_URL + '/auth/refresh', method: 'POST', enabled: true, interval: 30},
   rolesVar: 'role',
-  fetchData: {url: 'https://skibo.duckdns.org/tanktestapi/auth/user', method: 'GET', enabled: false},
+  fetchData: {url: BASE_URL + '/auth/user', method: 'GET', enabled: false},
   authRedirect: {path: '/'}
   // loginData: {url: '/auth/login'}
   // _parseUserData: function (data) {
