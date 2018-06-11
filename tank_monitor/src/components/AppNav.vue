c44e0d588b9444c317bc063893f91b29dc409ae8
Marcus <best.nose@gmail.com>
2018-06-11 18:02:57 +1200

    tidy up

--- tank_monitor/src/components/AppNav.vue
+++ tank_monitor/src/components/AppNav.vue
@@ -1,32 +1,5 @@
 <template>
     <div class="menu">
-      <!-- <span v-show="!$auth.check()">
-        <tree
-          :data="nonauthtreeData"
-          @node:selected="onNodeSelected"
-        />
-      </span> -->
-      <!-- <span v-if="$auth.check()">
-        <span v-if="$auth.check('admin')">
-          <tree
-            :data="authtreeData"
-            class="tree--small"
-            @node:selected="this.onNodeSelected"
-          />
-        </span>
-        <span v-if="$auth.check('user')">
-          <tree
-            :data="this.usertreeData"
-            @node:selected="onNodeSelected"
-          />
-        </span>
-      </span>
-      <span v-if="$auth.check(undefined)">
-        <tree
-          :data="this.nonauthtreeData"
-          @node:selected="onNodeSelected"
-        />
-      </span> -->
       <span>
         <tree
           :data="authtreeData"
@@ -63,28 +36,10 @@ export default {
         this.$router.push({name: 'Login'})
       } else {
         this.$router.push({name: node.text})
-        // this.$router.push('/auth/login')
-        // router.push({name: node.text})
       }
     }
-    // checkCreds () {
-    //   if (!this.$auth.check()) {
-    //     this.nonauthtreeData = [{text: 'Login'}]
-    //   } else {
-    //     this.nonauthtreeData = []
-    //     this.authtreeData = [{text: 'TankLord', children: [{text: 'Graphs'}, {text: 'Logout'}, {text: 'ManageUsers'}, {text: 'ManageTanks'}]}]
-    //     this.usertreeData = [{text: 'TankLord', children: [{text: 'Graphs'}, {text: 'Logout'}]}]
-    //   }
-    // }
   },
-  // created () {
-  //   this.checkCreds()
-  // },
-  // beforeCreate () {
-  //   this.nonauthtreeData = [{text: 'Login'}]
-  // },
   components: {
-    // [tree-nav.name]: LiquorTree
   },
   data: () => ({
     authtreeData: [
@@ -97,24 +52,10 @@ export default {
         ]
       }
     ]
-    // usertreeData: [
-    //   {text: 'TankLord',
-    //     children: [
-    //       {text: 'Graphs'},
-    //       {text: 'Logout'}
-    //     ]
-    //   }
-    // ],
-    // nonauthtreeData: [{text: 'Login'}]
   })
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/*.navbar-right { margin-right: 0px !important}
-
-.log {
-  margin: 5px 10px 0 0;
-}*/
 </style>
