55006bc31d8937a1b695c204feb2296dd20572b0
Marcass <best.nose@gmail.com>
2018-09-01 16:54:49 +1200

    debug push

--- python/api/monitor.py
+++ python/api/monitor.py
@@ -255,29 +255,29 @@ def sort_data(data):
     #     print 'malformed string'
 
 def readlineCR(port):
-    try:
-        rv = ''
-        while True:
-            ch = port.read()
-            rv += ch
-            if ch=='\n':# or ch=='':
+    # try:
+    rv = ''
+    while True:
+        ch = port.read()
+        rv += ch
+        if ch=='\n':# or ch=='':
+            print rv
+            if 'PY' in rv:              #arduino formats message as PY;<nodeID>;<waterlevel>;<batteryvoltage>;>\r\n
+                #print 'Printing status flags stuff on receive'
+                #for x in tanks.tank_list:
+                    #print x.name +' is ' +x.statusFlag
                 print rv
-                if 'PY' in rv:              #arduino formats message as PY;<nodeID>;<waterlevel>;<batteryvoltage>;>\r\n
-                    #print 'Printing status flags stuff on receive'
-                    #for x in tanks.tank_list:
-                        #print x.name +' is ' +x.statusFlag
-                    print rv
-                    rec_split = rv.split(';')   #make array like [PYTHON, nodeID, payloadance]
-                    print rec_split
-                    sort_data(rec_split[1:4])
-                    #q.put(rec_split[1:4])           #put data in queue for processing at rate
-                    rv = ''
-    except (KeyboardInterrupt, SystemExit):
-        print "Interrupted"
-        sys.exit()
-    except:
-        print 'failed on port read'
-        port_start()
+                rec_split = rv.split(';')   #make array like [PYTHON, nodeID, payloadance]
+                print rec_split
+                sort_data(rec_split[1:4])
+                #q.put(rec_split[1:4])           #put data in queue for processing at rate
+                rv = ''
+    # except (KeyboardInterrupt, SystemExit):
+    #     print "Interrupted"
+    #     sys.exit()
+    # except:
+    #     print 'failed on port read'
+    #     port_start()
 
 def readlineCR_test(port):
     rv = ''
