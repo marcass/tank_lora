4746420ec1c9bedd5f0cef3f4684e7ee34432811
Marcass <best.nose@gmail.com>
2018-09-01 17:38:13 +1200

    serial fucking with me

--- python/api/monitor.py
+++ python/api/monitor.py
@@ -262,20 +262,21 @@ def readlineCR(port):
     # try:
     rv = ''
     while True:
-        ch = port.read()
-        rv += ch
-        if ch=='\n':# or ch=='':
-            print rv
-            if 'PY' in rv:              #arduino formats message as PY;<nodeID>;<waterlevel>;<batteryvoltage>;>\r\n
-                #print 'Printing status flags stuff on receive'
-                #for x in tanks.tank_list:
-                    #print x.name +' is ' +x.statusFlag
+        if port.in_waiting:
+            ch = port.read()
+            rv += ch
+            if ch=='\n':# or ch=='':
                 print rv
-                rec_split = rv.split(';')   #make array like [PYTHON, nodeID, payloadance]
-                print rec_split
-                sort_data(rec_split[1:4])
-                #q.put(rec_split[1:4])           #put data in queue for processing at rate
-                rv = ''
+                if 'PY' in rv:              #arduino formats message as PY;<nodeID>;<waterlevel>;<batteryvoltage>;>\r\n
+                    #print 'Printing status flags stuff on receive'
+                    #for x in tanks.tank_list:
+                        #print x.name +' is ' +x.statusFlag
+                    print rv
+                    rec_split = rv.split(';')   #make array like [PYTHON, nodeID, payloadance]
+                    print rec_split
+                    sort_data(rec_split[1:4])
+                    #q.put(rec_split[1:4])           #put data in queue for processing at rate
+                    rv = ''
     # except (KeyboardInterrupt, SystemExit):
     #     print "Interrupted"
     #     sys.exit()
